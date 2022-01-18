b5daea1004b85276a5280d55a485f135f3d58f60
adambehrendt <adam.behrendt@gatech.edu>
2022-01-18 17:15:42 -0500

    Uncommented Code

---
+++ sample_example.py
@@ -0,0 +1,60 @@
+import utils
+import numpy as np
+
+
+def example(inhomogeneous=False):
+	T = 720
+	S = 60
+
+	num_pickup_bins = 48
+	num_periods = 26
+
+	radius = 20
+	num_origins = 4
+	origin_list = utils.make_origins(num_origins, radius)
+	origin_rv = stats.randint(0, num_origins)
+
+	K = 100
+
+	# Make K realizations for a random row (jth) of the dataset
+	if not inhomogeneous:
+		X = genfromtxt('data_homogeneous.csv', delimiter=',')
+	else:
+		X = genfromtxt('data_inhomogeneous.csv', delimiter=',')
+
+	j = stats.randint(0, len(X))
+
+	pickup_histogram = X[j, 0:num_pickup_bins]
+	d_mean = X[j, num_pickup_bins]
+	d_std = X[j, num_pickup_bins+1]
+	N_mean = X[j, num_pickup_bins+2]
+	N_std = X[j, num_pickup_bins+3]
+	num_known = X[j, num_pickup_bins+4]
+	if not inhomogeneous:
+		actual_lambda = X[j, num_pickup_bins+5]
+			
+	else:
+		actual_lambda = sub_X_test[j, num_pickup_bins+5:num_pickup_bins+5+num_periods]
+
+	const_rate = actual_lambda/((T+S)/num_periods)
+
+	a, b, mu, sigma = utils.make_bounds(0, np.inf, d_mean, d_std)
+	distance_rv = stats.truncnorm(a, b, loc=mu, scale=sigma)
+	a, b, mu, sigma = utils.make_bounds(0, np.inf, N_mean, N_std)
+	N_rv = stats.truncnorm(a, b, loc=mu, scale=sigma)
+
+	realization_set = [None for _ in range(evaluation_size)]
+	ad_hoc_set = [None for _ in range(evaluation_size)]
+	for k in range(K):
+		order_list = utils.gen_random_order_list(N_rv, 45, pickup_histogram, S, distance_rv, origin_rv, origin_list, num_known, placement_is_pickup=False)
+		order_list.sort(key=lambda x: x.placement_time)
+		realization_set[k] = order_list
+		if Lambda == 0:
+			pass 
+		else:
+			if not inhomogeneous:
+				ad_hoc_set[k] = utils.gen_poisson_arrivals(const_rate, T+S, start=0)
+			else:
+				ad_hoc_set[k] = utils.gen_inhomogeneous_arrivals(const_rate, T+S, num_periods)
+
+	return realization_set, ad_hoc_set
--- utils.py
+++ utils.py
@@ -3,6 +3,25 @@ import scipy.stats as stats
 import random
 
 
+class Order():
+    def __init__(self, placement_time, pickup_time, deadline, origin_idx, pickup_loc, delivery_loc, index):
+        self.placement_time = placement_time
+        self.pickup_time = pickup_time
+        self.deadline = deadline
+        self.origin_idx = origin_idx
+        self.pickup_loc = pickup_loc
+        self.delivery_loc = delivery_loc
+        self.actual_pickup_time = None
+        self.actual_delivery_time = None
+        self.index = index
+        self.assigned = False
+        self.attempted_vehicle_inserts = []
+
+
+def make_bounds(a, b, mean, std):
+    return (a - mean) / std, (b - mean) / std, mean, std
+
+
 def gen_poisson_arrivals(rate, T, start=0):
     k = 0
     t = start
@@ -29,27 +48,27 @@ def gen_inhomogeneous_arrivals(rate, T, num_periods):
     return arrivals
 
 
-class Order():
-    def __init__(self, placement_time, pickup_time, deadline, origin_idx, pickup_loc, delivery_loc, index):
-        self.placement_time = placement_time
-        self.pickup_time = pickup_time
-        self.deadline = deadline
-        self.origin_idx = origin_idx
-        self.pickup_loc = pickup_loc
-        self.delivery_loc = delivery_loc
-        self.actual_pickup_time = None
-        self.actual_delivery_time = None
-        self.index = index
-        self.assigned = False
-        self.attempted_vehicle_inserts = []
+def make_origins(num_origins, r):
+    if num_origins == 1:
+        return [(0,0)]
+    else:
+        origin_list = [None for _ in range(num_origins)]
+        theta = (360/num_origins)*(np.pi/180)
+        for i in range(num_origins):
+            angle = (i+1)*theta
+            origin_list[i] = (np.cos(angle)*(r/2), np.sin(angle)*(r/2))
+
+    return origin_list
 
 
-def gen_random_order_list(N_rv, placement_rv, pickup_histogram, window_rv, distance_rv, origin_rv, origin_list, num_known, placement_is_pickup=False):
+def gen_random_order_list(N_rv, placement_rv, pickup_histogram, window_rv, distance_rv, origin_rv, origin_list, num_known):
     num_known = int(num_known)
+
     if type(N_rv) != int:
         N = int(N_rv.rvs(size=1)) + num_known
     else:
         N = N_rv + num_known
+
     period_times = [15*i for i in range(len(pickup_histogram))]
     pickup_rv = stats.rv_discrete(name='pickup', values=(period_times, pickup_histogram))
     temp_pickup_times = pickup_rv.rvs(size=N)
@@ -60,10 +79,12 @@ def gen_random_order_list(N_rv, placement_rv, pickup_histogram, window_rv, dista
         placement_times = placement_rv.rvs(size=N)
     else:
         placement_times = [max(0, pickup_times[i] - placement_rv) for i in range(N)]
+
     if type(window_rv) != int:
         window_lengths = window_rv.rvs(size=N)
     else: 
         window_lengths = [window_rv for _ in range(N)]
+
     distance_vals = distance_rv.rvs(size=N)
     origin_vals = origin_rv.rvs(size=N)
     origin_locs = [origin_list[index] for index in origin_vals]
@@ -72,13 +93,9 @@ def gen_random_order_list(N_rv, placement_rv, pickup_histogram, window_rv, dista
         rand_angle = random.random()*np.pi*2
         delivery_locs[i] = (origin_locs[i][0]+np.cos(rand_angle)*distance_vals[i], origin_locs[i][1]+np.sin(rand_angle)*distance_vals[i])
 
-
     order_list = [None for _ in range(N)]
     for i in range(N):
-        if placement_is_pickup is True:
-            order_list[i] = Order(placement_times[i], placement_times[i], placement_times[i]+window_lengths[i], origin_vals[i], origin_locs[i], delivery_locs[i], i)
-        else:
-            order_list[i] = Order(placement_times[i], pickup_times[i], pickup_times[i]+window_lengths[i], origin_vals[i], origin_locs[i], delivery_locs[i], i)
+        order_list[i] = Order(placement_times[i], pickup_times[i], pickup_times[i]+window_lengths[i], origin_vals[i], origin_locs[i], delivery_locs[i], i)
 
     known_idx = random.sample(range(N), num_known)
     for idx in known_idx:
