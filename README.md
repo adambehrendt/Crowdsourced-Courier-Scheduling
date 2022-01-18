6d1ea59011bfb8abcbd153c19105376e9d6d77ac
adambehrendt <adam.behrendt@gatech.edu>
2022-01-18 16:11:27 -0500

    Editing README and adding data files

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Crowdsourced-Courier-Scheduling
 
 ## Description
-This is the official data repository for the paper entitled "A Prescriptive Machine Learning Method for Courier Scheduling on Crowdsourced Delivery Platforms" submitted to Transportation Science [Optimization Online Link](http://www.optimization-online.org/DB_HTML/2021/10/8661.html). In this repository we include the datasets that we used in the paper that has been generated from real world crowdsourced delivery demand data according to our bootstrap sampling procedure, and hence is anonomized.
+This is the official data repository for the paper entitled "A Prescriptive Machine Learning Method for Courier Scheduling on Crowdsourced Delivery Platforms" submitted to Transportation Science [(Optimization Online Link)](http://www.optimization-online.org/DB_HTML/2021/10/8661.html). In this repository we include the datasets that we used in the paper that has been generated from real world crowdsourced delivery demand data according to our bootstrap sampling procedure, and hence is anonymized.
 
 ## Data
+The data is stored in ```data_homogenous.csv``` and ```data_inhomogeneous.csv``` for homogeneous and inhomogeneous Poisson arrival processes, respectively. Each row of the ```*.csv``` files contains information describing the distributions of demand and ad-hoc courier arrivals for an operational period. That is, each row is to be used to generate multiple realizations of sets of orders. For a given row, the first 48 entries are what we refer to as the "pickup histogram" where each entry represents a 15 minute block of time and the probability that any individual order will have a pickup time in that time block (as such, the sum of the values is 1). The folling two entries are the mean and standard deviation of the distance between the O-D pairs of orders, following a normal distribution truncated at 0. Similarly, the next two entries are the mean and standard deviation of the total number of orders expected to arrive dynamically over the time horizon, following a normal distribution truncated at 0. The following entry is the final entry which describes demand and is the exact number of orders known at the beginning of the time horizon (i.e. the number of static orders). 
 
-### Homogeneous Arrivals
+As for ad-hoc courier arrivals, we consider both homogeneous and inhomogeneous Poisson arrival processes and hence have a single additional feature representing the average arrival rate per period in the homogenous case and a feature vector of size 26 for the inhomogenous case, representing the arrival rate for each individual period.
 
-### Inhomogeneous Arrivals
+For more detailed information on the structure of this data, how it was generated and our assumptions, please refer to the "Data Description, Processing and Assumptions" section of our paper.
 
 ## Sampling the Data
---
+++ order_generator.py
@@ -0,0 +1,87 @@
+import numpy as np
+import scipy.stats as stats
+import random
+
+
+def gen_poisson_arrivals(rate, T, start=0):
+    k = 0
+    t = start
+    arrivals = []
+    while True:
+        r = np.random.uniform()
+        t = t - (np.log(r)/rate)
+        if t > T+start:
+            break
+        else:
+            k = k+1
+            arrivals.append(t)
+
+    return arrivals
+
+
+def gen_inhomogeneous_arrivals(rate, T, num_periods):
+    period_length = T/num_periods
+    arrivals = []
+    for p in range(num_periods):
+        temp = gen_poisson_arrivals(rate[p], period_length, period_length*p)
+        arrivals.extend(temp)
+
+    return arrivals
+
+
+class Order():
+    def __init__(self, placement_time, pickup_time, deadline, origin_idx, pickup_loc, delivery_loc, index):
+        self.placement_time = placement_time
+        self.pickup_time = pickup_time
+        self.deadline = deadline
+        self.origin_idx = origin_idx
+        self.pickup_loc = pickup_loc
+        self.delivery_loc = delivery_loc
+        self.actual_pickup_time = None
+        self.actual_delivery_time = None
+        self.index = index
+        self.assigned = False
+        self.attempted_vehicle_inserts = []
+
+
+def gen_random_order_list(N_rv, placement_rv, pickup_histogram, window_rv, distance_rv, origin_rv, origin_list, num_known, placement_is_pickup=False):
+    num_known = int(num_known)
+    if type(N_rv) != int:
+        N = int(N_rv.rvs(size=1)) + num_known
+    else:
+        N = N_rv + num_known
+    period_times = [15*i for i in range(len(pickup_histogram))]
+    pickup_rv = stats.rv_discrete(name='pickup', values=(period_times, pickup_histogram))
+    temp_pickup_times = pickup_rv.rvs(size=N)
+    add_term = np.random.randint(0, 15, N)
+    pickup_times = temp_pickup_times + add_term
+
+    if type(placement_rv) != int:
+        placement_times = placement_rv.rvs(size=N)
+    else:
+        placement_times = [max(0, pickup_times[i] - placement_rv) for i in range(N)]
+    if type(window_rv) != int:
+        window_lengths = window_rv.rvs(size=N)
+    else: 
+        window_lengths = [window_rv for _ in range(N)]
+    distance_vals = distance_rv.rvs(size=N)
+    origin_vals = origin_rv.rvs(size=N)
+    origin_locs = [origin_list[index] for index in origin_vals]
+    delivery_locs = [None for _ in range(N)]
+    for i in range(N):
+        rand_angle = random.random()*np.pi*2
+        delivery_locs[i] = (origin_locs[i][0]+np.cos(rand_angle)*distance_vals[i], origin_locs[i][1]+np.sin(rand_angle)*distance_vals[i])
+
+
+    order_list = [None for _ in range(N)]
+    for i in range(N):
+        if placement_is_pickup is True:
+            order_list[i] = Order(placement_times[i], placement_times[i], placement_times[i]+window_lengths[i], origin_vals[i], origin_locs[i], delivery_locs[i], i)
+        else:
+            order_list[i] = Order(placement_times[i], pickup_times[i], pickup_times[i]+window_lengths[i], origin_vals[i], origin_locs[i], delivery_locs[i], i)
+
+    known_idx = random.sample(range(N), num_known)
+    for idx in known_idx:
+        order_list[idx].placement_time = 0
+
+    return order_list
